c9da239fbf113065787245b2c91fa4392361608b
Kevin Laguel <laguel.kev@gmail.com>
2022-02-08 18:42:28 +0100

    ajout details page show

--- src/views/Show.vue
+++ src/views/Show.vue
@@ -1,29 +1,32 @@
 <template>
-    <div class="shadow-lg bg-white rounded-lg relative">
-        <div class="flex justify-center items-center rounded-full w-12 h-12 bg-rose-600 rotate-12 absolute top-0 right-0">
-            <span class="font-bold text-white"> {{ flag }} </span>
-        </div>
-        <div class="card-image p-4 flex justify-center">
+    <div class="flex bg-white rounded-lg relative">
+      <div class="w-1/3">
+        <div class=" relative card-image p-4 flex justify-center">
+            <div class="flex justify-center items-center rounded-full w-12 h-12 bg-rose-600 rotate-12 absolute top-0 right-0">
+              <span class="font-bold text-white"> {{ flag }} </span>
+            </div>
             <figure class="image is-4by3">
             <img :src="imgUrl">
             </figure>
         </div>
-        <div class="card-content p-4">
+      </div>
+      <div>
+        <div class="card-content p-4 w-2/3">
           <h3>
-              {{ title }}
+             <span class="font-bold">Nom:</span> {{ title }}
           </h3>
            <h3>
-              {{ price }}
+              <span class="font-bold">Prix:</span> {{ price }}
           </h3>
           <h3>
-              {{ description }}
+              <span class="font-bold">Description:</span> {{ description }}
           </h3>
         </div>
+      </div>
     </div>
 </template>
 
 <script>
-
 
 export default {
   name: 'Show',
